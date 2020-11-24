ababd6f061d099bc4858037609165934a9a509c3
Sagnik Bhattacharya <sagnikbhattacharya1506@gmail.com>
2020-11-24 21:16:00 +0530

    Class

---
+++ flutter_app/lib/services/responsive_handler.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Services extends StatefulWidget {
+  @override
+  _ServicesState createState() => _ServicesState();
+}
+
+class _ServicesState extends State<Services> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            leading: Text("hello world",style: TextStyle(fontSize: 20.0),),
+          ),
+        )
+        ,
+      ),
+
+    );
+  }
+}
