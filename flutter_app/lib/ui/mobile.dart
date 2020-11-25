e2b23ce51379600fe30c3504846606f34e68ad8c
Sagnik Bhattacharya <sagnikbhattacharya1506@gmail.com>
2020-11-25 17:50:58 +0530

    ...

--- flutter_app/lib/ui/mobile.dart
+++ flutter_app/lib/ui/mobile.dart
@@ -4,6 +4,8 @@ import 'package:flutter/cupertino.dart';
 class Mobile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text("Hello1");
+    return FittedBox(
+      fit: BoxFit.contain,
+        child: Text("Hello1"));
   }
 }
