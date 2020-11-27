0beb58e8ffd4d8549fd289b856c1fbfcc9812c53
Sagnik Bhattacharya <sagnikbhattacharya1506@gmail.com>
2020-11-27 23:45:13 +0530

    ..

--- flutter_app/lib/ui/mobile.dart
+++ flutter_app/lib/ui/mobile.dart
@@ -42,90 +42,99 @@ class _Mobile extends State<Mobile> {
       ),
     ];
 
+
+controller.addListener(() {
+  closeTopContainer = controller.offset > 50;
+});
+
     List<Widget> itemData = [
-      Stack(children: <Widget>[
-        Padding(
-            padding: const EdgeInsets.all(0),
-            child: CarouselSlider(
-                items: item,
-                options: CarouselOptions(
-                  height: 500,
-                  aspectRatio: 16 / 9,
-                  viewportFraction: 0.911,
-                  initialPage: 0,
-                  enableInfiniteScroll: true,
-                  reverse: false,
-                  autoPlay: true,
-                  autoPlayInterval: Duration(seconds: 3),
-                  autoPlayAnimationDuration: Duration(milliseconds: 800),
-                  autoPlayCurve: Curves.easeInOutSine,
-                  enlargeCenterPage: true,
-                  scrollDirection: Axis.horizontal,
-                ))),
-        Padding(
-          padding: const EdgeInsets.only(top: 5),
-          child: ListTile(
-            title: Text(
-              "",
-              style: GoogleFonts.oswald(fontSize: 40),
-            ),
-            leading: IconButton(
-              icon: Icon(
-                Icons.menu,
-                size: 40,
-              ),
-              onPressed: () {
-                // Scaffold.of(context).openDrawer();
-              },
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(top: 450, left: 30),
-          child: Card(
-            elevation: 10,
-            color: Colors.black,
-            child: Text(
-              "Krala Murnau",
-              style: GoogleFonts.oswald(fontSize: 45, color: Colors.white),
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(top: 540, left: 125),
-          child: Card(
-            shadowColor: Colors.black,
-            elevation: 0,
-            child: Text(
-              "Private Detective",
-              style: GoogleFonts.sedgwickAveDisplay(fontSize: 35),
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(top: 550, left: 70),
-          child: Icon(
-            Icons.search,
-            size: 35,
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 135, top: 580),
-          child: Container(
-              child: Text("Current Location: $city",
-                  style: GoogleFonts.inconsolata(
-                      fontSize: 15,
-                      textStyle: TextStyle(color: Colors.black45)))),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 20, top: 625),
-          child: ListTile(
+      SizedBox(
+        height: 900,
+        width: 700,
+        child: Stack(children: <Widget>[
+          Padding(
+              padding: const EdgeInsets.all(0),
+              child: CarouselSlider(
+                  items: item,
+                  options: CarouselOptions(
+                    height: 500,
+                    aspectRatio: 16 / 9,
+                    viewportFraction: 0.911,
+                    initialPage: 0,
+                    enableInfiniteScroll: true,
+                    reverse: false,
+                    autoPlay: true,
+                    autoPlayInterval: Duration(seconds: 3),
+                    autoPlayAnimationDuration: Duration(milliseconds: 800),
+                    autoPlayCurve: Curves.easeInOutSine,
+                    enlargeCenterPage: true,
+                    scrollDirection: Axis.horizontal,
+                  ))),
+          Padding(
+            padding: const EdgeInsets.only(top: 5),
+            child: ListTile(
               title: Text(
-            "Providing private detective services and private investigation services to businesses ",
-            style: GoogleFonts.inconsolata(color: Colors.black38),
-          )),
-        ),
-      ]),
+                "",
+                style: GoogleFonts.oswald(fontSize: 40),
+              ),
+              leading: IconButton(
+                icon: Icon(
+                  Icons.menu,
+                  size: 40,
+                ),
+                onPressed: () {
+                  // Scaffold.of(context).openDrawer();
+                },
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 450, left: 30),
+            child: Card(
+              elevation: 10,
+              color: Colors.black,
+              child: Text(
+                "Krala Murnau",
+                style: GoogleFonts.oswald(fontSize: 45, color: Colors.white),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 540, left: 125),
+            child: Card(
+              shadowColor: Colors.black,
+              elevation: 0,
+              child: Text(
+                "Private Detective",
+                style: GoogleFonts.sedgwickAveDisplay(fontSize: 35),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 550, left: 70),
+            child: Icon(
+              Icons.search,
+              size: 35,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 135, top: 580),
+            child: Container(
+                child: Text("Current Location: $city",
+                    style: GoogleFonts.inconsolata(
+                        fontSize: 15,
+                        textStyle: TextStyle(color: Colors.black45)))),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 20, top: 625),
+            child: ListTile(
+                title: Text(
+              "Providing private detective services and private investigation services to businesses ",
+              style: GoogleFonts.inconsolata(color: Colors.black38),
+            )),
+          ),
+        ]),
+      ),
       SizedBox(
         height: 50,
       ),
@@ -299,6 +308,7 @@ class _Mobile extends State<Mobile> {
         ),
       ]),
     ];
+
 
     return SafeArea(
       child: Scaffold(
