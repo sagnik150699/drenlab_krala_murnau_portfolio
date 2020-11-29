b5f82329343391181ff7a9ed72f1d3323ba2ef9f
Sagnik Bhattacharya <sagnikbhattacharya1506@gmail.com>
2020-11-29 21:36:31 +0530

    ..

--- flutter_app/lib/ui/mobilesmall.dart
+++ flutter_app/lib/ui/mobilesmall.dart
@@ -28,99 +28,42 @@ class _Mobile2 extends State<Mobile2> {
     });
 
     List<Widget> itemData = [
-      AnimatedContainer(
-        duration: const Duration(microseconds: 200),
-        width: size.width,
-        alignment: Alignment.topCenter,
-        height: closeTopContainer ? 0 : categoryHeight,
-        child: SizedBox(
-          height: 900,
-          width: 700,
-          child: Stack(children: <Widget>[
-            Padding(
-                padding: const EdgeInsets.all(0),
-                child: CarouselSlider(
-                    items: Variables().item,
-                    options: CarouselOptions(
-                      height: 500,
-                      aspectRatio: 16 / 9,
-                      viewportFraction: 0.911,
-                      initialPage: 0,
-                      enableInfiniteScroll: true,
-                      reverse: false,
-                      autoPlay: true,
-                      autoPlayInterval: Duration(seconds: 3),
-                      autoPlayAnimationDuration: Duration(milliseconds: 800),
-                      autoPlayCurve: Curves.easeInOutSine,
-                      enlargeCenterPage: true,
-                      scrollDirection: Axis.horizontal,
-                    ))),
-            // Padding(
-            //   padding: const EdgeInsets.only(top: 5),
-            //   child: ListTile(
-            //     title: Text(
-            //       "",
-            //       style: GoogleFonts.oswald(fontSize: 40),
-            //     ),
-            //     leading: IconButton(
-            //       icon: Icon(
-            //         Icons.menu,
-            //         size: 40,
-            //       ),
-            //       onPressed: () {
-            //          Scaffold.of(context).openDrawer();
-            //       },
-            //     ),
-            //   ),
-            // ),
-            Padding(
-              padding: const EdgeInsets.only(top: 450, left: 30),
-              child: Card(
-                elevation: 10,
-                color: Colors.black,
-                child: Text(
-                  "Krala Murnau",
-                  style: GoogleFonts.oswald(fontSize: 45, color: Colors.white),
-                ),
-              ),
+      Stack(children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(top: 0, left: 125),
+          child: Card(
+            shadowColor: Colors.black,
+            elevation: 0,
+            child: Text(
+              "Private Detective",
+              style: GoogleFonts.sedgwickAveDisplay(fontSize: 35),
             ),
-            Padding(
-              padding: const EdgeInsets.only(top: 540, left: 125),
-              child: Card(
-                shadowColor: Colors.black,
-                elevation: 0,
-                child: Text(
-                  "Private Detective",
-                  style: GoogleFonts.sedgwickAveDisplay(fontSize: 35),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 550, left: 70),
-              child: Icon(
-                Icons.search,
-                size: 35,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 135, top: 580),
-              child: Container(
-                  child: Text("Current Location: $city",
-                      style: GoogleFonts.inconsolata(
-                          fontSize: 15,
-                          textStyle: TextStyle(color: Colors.black45)))),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 20, top: 625),
-              child: ListTile(
-                  title: Text(
-                    "Providing private detective services and private investigation services to businesses ",
-                    style: GoogleFonts.inconsolata(color: Colors.black38),
-                  )),
-            ),
-          ]),
+          ),
         ),
-      ),
+        Padding(
+          padding: const EdgeInsets.only(top: 10, left: 70),
+          child: Icon(
+            Icons.search,
+            size: 35,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 135, top: 40),
+          child: Container(
+              child: Text("Current Location: $city",
+                  style: GoogleFonts.inconsolata(
+                      fontSize: 15,
+                      textStyle: TextStyle(color: Colors.black45)))),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 20, top: 80),
+          child: ListTile(
+              title: Text(
+                "Providing private detective services and private investigation services to businesses ",
+                style: GoogleFonts.inconsolata(color: Colors.black38),
+              )),
+        ),
+      ]),
       Padding(
         padding: const EdgeInsets.only(right: 20, left: 20),
         child: Container(
@@ -355,17 +298,17 @@ class _Mobile2 extends State<Mobile2> {
           ),
         ),
         key: _scaffoldKey,
-        appBar: AppBar(
-          backgroundColor: Colors.black,
-          leading: new IconButton(
-            color: Colors.white,
-            icon: new Icon(
-              Icons.menu,
-              size: 40,
-            ),
-            onPressed: () => _scaffoldKey.currentState.openDrawer(),
-          ),
-        ),
+        // appBar: AppBar(
+        //   backgroundColor: Colors.black,
+        //   leading: new IconButton(
+        //     color: Colors.white,
+        //     icon: new Icon(
+        //       Icons.menu,
+        //       size: 40,
+        //     ),
+        //     onPressed: () => _scaffoldKey.currentState.openDrawer(),
+        //   ),
+        // ),
         // floatingActionButton: Padding(
         //
         //   padding: const EdgeInsets.only( bottom: 670),
@@ -376,7 +319,59 @@ class _Mobile2 extends State<Mobile2> {
         //   ),
         // ),
 
-        body: ListView.builder(
+        body:
+        NestedScrollView(
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return <Widget>[
+              SliverAppBar(
+                leading: new IconButton(
+                  padding: EdgeInsets.only(top: 0,left: 7),
+                  color: Colors.black,
+                  icon: new Icon(
+                    Icons.menu,
+                    size: 40,
+                  ),
+                  onPressed: () => _scaffoldKey.currentState.openDrawer(),
+                ),
+                backgroundColor: Colors.white,
+                expandedHeight: 500.0,
+                floating: false,
+                pinned: true,
+                flexibleSpace: FlexibleSpaceBar(
+                  centerTitle: true,
+                  title: Padding(
+                    padding: const EdgeInsets.only(top: 0, right: 80),
+                    child: Card(
+                      elevation: 0,
+                      color: Colors.black,
+                      child: Text(
+                        "Krala Murnau",
+                        style: GoogleFonts.oswald(fontSize: 30, color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  background: CarouselSlider(
+                      items: Variables().item,
+                      options: CarouselOptions(
+                        height: 400,
+                        aspectRatio: 16 / 9,
+                        viewportFraction: 0.911,
+                        initialPage: 0,
+                        enableInfiniteScroll: true,
+                        reverse: false,
+                        autoPlay: true,
+                        autoPlayInterval: Duration(seconds: 3),
+                        autoPlayAnimationDuration: Duration(milliseconds: 800),
+                        autoPlayCurve: Curves.easeInOutSine,
+                        enlargeCenterPage: true,
+                        scrollDirection: Axis.horizontal,
+                      )),
+                ),
+              )
+            ];
+          },
+          body:
+          ListView.builder(
           controller: controller,
           physics: BouncingScrollPhysics(),
           addAutomaticKeepAlives: false,
@@ -386,8 +381,21 @@ class _Mobile2 extends State<Mobile2> {
             return itemData[index];
           },
         ),
+        ),
       ),
     );
     // return
   }
 }
+
+
+
+
+
+
+
+
+// onVerticalDragUpdate: (DragUpdateDetails details){  print(details);},
+// onVerticalDragStart: (DragStartDetails details){
+//   print(details);
+// },
