e4fa4b560a54ee22095f676edd7a9cad29f1c932
Sagnik Bhattacharya <32522431+sagnik150699@users.noreply.github.com>
2020-12-30 18:32:37 +0530

    .

--- flutter_app/lib/ui/blogmobile.dart
+++ flutter_app/lib/ui/blogmobile.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
-import 'package:flutter_app/components/v';
+import 'package:flutter_app/components/variables.dart';
 import 'package:google_fonts/google_fonts.dart';
 //hellosss
 class Mobile extends StatefulWidget {
